9ebdcadcf49878473c6db69bad3e345ccbc1afc7
AntoineBrih <toinebrihaye@gmail.com>
2025-08-26 15:55:17 +0200

    wip

--- Serie1/Ex1/Index.php
+++ Serie1/Ex1/Index.php
@@ -1,14 +1,5 @@
-<!doctype html>
- <html>
- <head>
- </head>
- <body>
- <h1>Bienvenue !</h1> <p>Il est actuellement
- <?php
- $heure = date('H:i');
- echo $heure;
- ?>
-    et 
-tout va très bien.</p>
- </body>
- </html>
+<?php
+
+echo"Coucou petite perruche";
+
+?>
